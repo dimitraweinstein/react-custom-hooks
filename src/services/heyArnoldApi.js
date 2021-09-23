b5c4b7b209633bf18fe97a1503ce32c2e13d3de4
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-23 09:51:24 -0700

    created fetchbyid function

--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -10,3 +10,15 @@ export const fetchHeyArnold = async () => {
   })));
 };
 
+export const fetchHeyArnoldById = async (id) => {
+  const res = await fetch(
+    `https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`
+  );
+  const json = await res.json();
+  return json.map((character) => ({
+    id: character._id,
+    name: character.name,
+    image: character.image,
+  }));
+};
+
