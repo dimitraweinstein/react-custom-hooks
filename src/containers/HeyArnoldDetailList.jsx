50ff181b04972a60b7af65314185c9983276c85a
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-23 15:21:12 -0700

    built character details container

---
+++ src/containers/HeyArnoldDetailList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { fetchHeyArnoldById } from '../services/heyArnoldApi';
+import Character from '../components/characters/Character';
+
+const HeyArnoldDetails = () => {
+  const [loading, setLoading] = useState(true);
+  const [character, setCharacter] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => (
+    fetchHeyArnoldById(id)
+      .then((character) => {
+        setCharacter(character);
+        console.log(character, 'this is a character');
+      })
+      .finally(() => setLoading(false))
+  ), []);
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <>
+      <Character
+        image={character.image}
+        name={character.name}
+      />
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+    </>
+  );
+};
+
+export default HeyArnoldDetails;
